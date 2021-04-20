f876b1ea522e708d486c8f27063b7255d428fb60
Juliano Ventola <ventolajuliano@gmail.com>
2021-04-20 02:09:18 -0300

    feat: Appbar and score card

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -13,7 +13,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(),
+      appBar: AppBarWidget(context),
     );
   }
 }
--- lib/home/widgets/appbar/appbar_widget.dart
+++ lib/home/widgets/appbar/appbar_widget.dart
@@ -1,37 +1,57 @@
 import 'package:devquiz/core/app_gradients.dart';
 import 'package:devquiz/core/app_text_styles.dart';
+import 'package:devquiz/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/cupertino.dart';
 
 class AppBarWidget extends PreferredSize {
-  AppBarWidget()
+  AppBarWidget(context)
       : super(
-          preferredSize: Size.fromHeight(250),
+          preferredSize: Size.fromHeight(MediaQuery.of(context).padding.top +
+              MediaQuery.of(context).size.height * 0.30),
           child: Container(
-            height: 250,
-            decoration: BoxDecoration(gradient: AppGradients.linear),
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            height: MediaQuery.of(context).padding.top +
+                MediaQuery.of(context).size.height * 0.30,
+            child: Stack(
               children: [
-                Text.rich(
-                  TextSpan(text: 'Olá ', style: AppTextStyles.title, children: [
-                    TextSpan(
-                      text: 'Juliano',
-                      style: AppTextStyles.titleBold,
-                    ),
-                  ]),
-                ),
                 Container(
-                  width: 58,
-                  height: 58,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        'https://avatars.githubusercontent.com/u/28475558?v=4',
+                  height: MediaQuery.of(context).padding.top +
+                      MediaQuery.of(context).size.height * 0.18,
+                  width: double.maxFinite,
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(gradient: AppGradients.linear),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                            text: 'Olá ',
+                            style: AppTextStyles.title,
+                            children: [
+                              TextSpan(
+                                text: 'Juliano',
+                                style: AppTextStyles.titleBold,
+                              ),
+                            ]),
                       ),
-                    ),
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.12,
+                        height: MediaQuery.of(context).size.width * 0.12,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: NetworkImage(
+                              'https://avatars.githubusercontent.com/u/28475558?v=4',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
+                ),
+                Align(
+                  alignment: Alignment(0, 1),
+                  child: ScoreCardWidget(),
                 ),
               ],
             ),
